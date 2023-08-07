cee6c10b6ca748efde0fd448193c2e0f34d33138
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-08-08 00:15:51 +0700

    FooBarServiceProvider

---
+++ app/Providers/FooBarServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Providers;
+
+use App\Data\Bar;
+use App\Data\Foo;
+use App\Services\HelloService;
+use App\Services\HelloServiceIndonesia;
+use Illuminate\Support\ServiceProvider;
+
+class FooBarServiceProvider extends ServiceProvider
+{
+    // ini untuk kasus yang sederhana saja, kalo kompleks maka tidak bisa gunakan yang ini.
+    public array $singleton = [
+        HelloService::class => HelloServiceIndonesia::class
+    ];
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // disini kita harus melalukan registrasi dependency yang dibutuhkan ke service container
+        // dan jangan melakukan kode selain registrasi, jika tidak maka akan terjadi error dependency
+        $this->app->singleton(Foo::class, function ($app) {
+            return new Foo();
+        });
+
+        $this->app->singleton(Bar::class, function ($app) {
+            return new Bar($app->make(Foo::class));
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // disini kita bisa melakukan hal apapun yang diperlukan setelah proses registrasi dependency selesai
+
+    }
+}
