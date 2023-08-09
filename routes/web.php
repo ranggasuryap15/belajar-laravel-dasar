0a9aaa8af2879e1f02077b8a7e4999473b28dc8e
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-08-09 13:36:14 +0700

    route parameter

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,35 @@ Route::get('/pzn', function(){
     return "Programmer Zaman Now";
 });
 
+Route::fallback(function(){ // fallback = 404
+    return "Halaman tidak ditemukan";
+});
+
 // route view
 Route::view('/hello', 'hello', ['name' => 'Rangga']);
 Route::get('/hello-again', function() {
     return view('hello', ['name' => 'Rangga']);
 });;
+
+// Route Parameter
+Route::get('/products/{id}', function($productId) {
+    return "Product : " . $productId;
+});
+
+// Route Parameter double
+Route::get('/products/{product}/items/{item}', function($productId, $itemId) {
+    return "Product : " . $productId . ", Item : " . $itemId;
+});
+
+Route::get('/users/{id?}', function(string $userId = '404') {
+    return "Users $userId";
+});
+
+// Routing conflict itu akan diprioritaskan yang bagian atasnya
+Route::get('/conflict/{name}', function($name) {
+    return 'Conflict '. $name;
+});
+
+Route::get('/conflict/eko', function() {
+    return 'Conflict Eko Khannedy';
+});
