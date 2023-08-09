9881a9acbf9bcd85778208519971d3f6d82f251f
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-08-09 13:43:08 +0700

    named route

--- routes/web.php
+++ routes/web.php
@@ -34,16 +34,22 @@ Route::get('/hello-again', function() {
 // Route Parameter
 Route::get('/products/{id}', function($productId) {
     return "Product : " . $productId;
-});
+})->name('product.detail');
 
 // Route Parameter double
 Route::get('/products/{product}/items/{item}', function($productId, $itemId) {
     return "Product : " . $productId . ", Item : " . $itemId;
-});
+})->name('product.item.detail');
+
+Route::get('/categories/{id}', function($categoryId) {
+    return "Category $categoryId";
+})->name('category.detail');
 
 Route::get('/users/{id?}', function(string $userId = '404') {
     return "Users $userId";
-});
+})->name('user.detail');
+
+// keuntungan menggunakan named route adalah ketika kita membutuhkan informasi dari route tersebut, cukup sebutkan nama routenya
 
 // Routing conflict itu akan diprioritaskan yang bagian atasnya
 Route::get('/conflict/{name}', function($name) {
@@ -52,4 +58,14 @@ Route::get('/conflict/{name}', function($name) {
 
 Route::get('/conflict/eko', function() {
     return 'Conflict Eko Khannedy';
+});
+
+// Contoh penggunaan untuk named route
+Route::get('/produk/{id}', function($id) {
+    $link = route('product.detail', ['id' => $id]);
+    return "Link : $link";
+});
+
+Route::get('/produk-redirect/{id}', function($id) {
+    return redirect()->route('product.detail', ['id' => $id]);
 });
